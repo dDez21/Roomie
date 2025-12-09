772aeff1853883317f6a0dda5e6348f99151642b
dDez21 <davidemario21@gmail.com>
2025-12-09 18:29:57 +0100

    wef

--- app/src/main/java/com/example/roomieproject/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/roomieproject/activity/ForgotPasswordActivity.kt
@@ -5,17 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.credentials.CredentialManager
 import com.example.roomieproject.R
-import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var Email: TextInputLayout
+    private lateinit var txtEmail: TextInputEditText
+    private lateinit var sendEmailButton: MaterialButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge() //adatta layout
         setContentView(R.layout.activity_forgot_password) //layout da usare
-
 
         //collego a id del layout
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.forgotPassword)) { v, insets ->
@@ -24,4 +29,16 @@ class ForgotPasswordActivity : AppCompatActivity() {
             insets
         }
 
+        Email = requireNotNull(findViewById(R.id.Email)){
+            "Contenitore email (TextInputLayout) non trovato nel layout"
+        }
+
+        txtEmail = requireNotNull(findViewById(R.id.textEmail)){
+            "EditText email non trovato nel layout"
+        }
+
+        sendEmailButton = requireNotNull(findViewById(R.id.sendEmail)){
+            "Bottone crea account non trovato nel layout"
+        }
+    }
 }
